cda0e28d333435cf91424d0915a4865ea3b2ad70
Pham Anh Tien <anhtien13082005@gmail.com>
2024-08-29 21:40:43 +0700

    research added

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+REPO_NAME = "Book-Recommendation"
+AUTHOR_USER_NAME = "Tien"
+SRC_REPO = "books_recommender"
+LIST_OF_REQUIREMENTS = []
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author="Tien",
+    description="A small local packages for ML based books recommendations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PhamAnhTienn/Book-Recommendation",
+    author_email="",
+    packages=find_packages(),
+    license="MIT",
+    python_requires=">=3.8",
+    install_requires=LIST_OF_REQUIREMENTS
+)
